1188b0a997225d581b4fc83735386403d9338f62
Fenrir712 <1056034126@qq.com>
2020-07-30 03:16:58 +0800

    init web project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,137 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app" class="wrapper">
+		<header class="main-header">
+			<!-- Logo -->
+			<a href="../../index2.html" class="logo">
+				<!-- mini logo for sidebar mini 50x50 pixels -->
+				<span class="logo-mini"><b>A</b>LT</span>
+				<!-- logo for regular state and mobile devices -->
+				<span class="logo-lg"><b>ECS</b>by23</span>
+			</a>
+			<!-- Header Navbar: style can be found in header.less -->
+			<nav class="navbar navbar-static-top">
+				<!-- Sidebar toggle button-->
+				<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+			</nav>
+		</header> 
+
+		<!-- =============================================== -->
+
+		<!-- Left side column. contains the sidebar -->
+		<aside class="main-sidebar">
+			<!-- sidebar: style can be found in sidebar.less -->
+			<section class="sidebar">
+				<!-- Sidebar user panel -->
+				<div class="user-panel">
+					<div class="pull-left image">
+						<img src="./assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+					</div>
+					<div class="pull-left info">
+						<p>用户名</p>
+						<a href="#"><i class="fa fa-circle text-red"></i>注销</a>
+					</div>
+				</div>
+				<!-- sidebar menu: : style can be found in sidebar.less -->
+				<ul class="sidebar-menu" data-widget="tree">
+					<li class="header">MENU</li>
+			
+					<li>
+						<a href="#">
+							<i class="fa fa-th"></i> <span>商品管理</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+					</li>
+
+					<li>
+						<a href="../mailbox/mailbox.html">
+							<i class="fa fa-envelope"></i> <span>订单管理</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+					</li>
+					
+					<li>
+						<a href="#">
+							<i class="fa fa-edit"></i> <span>账号信息</span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+					</li>
+
+				</ul>
+			</section>
+			<!-- /.sidebar -->
+		</aside>
+
+		<!-- =============================================== -->
+
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper">
+			<!-- Content Header (Page header) -->
+			<section class="content-header">
+				<h1>
+					东软电子商务系统
+					<small>商户端</small>
+				</h1>
+				<ol class="breadcrumb">
+					<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+					<li><a href="#">Examples</a></li>
+					<li class="active">Blank page</li>
+				</ol>
+			</section>
+
+			<!-- Main content -->
+			<section class="content">
+
+				<!-- Default box -->
+				<div class="box">
+					<div class="box-header with-border">
+						<h3 class="box-title">标题</h3>
+					</div>
+					<div class="box-body">
+						Start creating your amazing application!
+						<br>test
+						<br>test
+						<br>test
+					</div>
+					<!-- /.box-body -->
+					<div class="box-footer">
+						Footer
+					</div>
+					<!-- /.box-footer-->
+				</div>
+				<!-- /.box -->
+
+			</section>
+			<!-- /.content -->
+		</div>
+		<!-- /.content-wrapper -->
+
+		<footer class="main-footer">
+			<div class="pull-right hidden-xs">
+				<b>Version</b> 1.0
+			</div>
+			<strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">Neusoft</a>.</strong> All rights
+			reserved.
+		</footer>
+
+	</div>
 </template>
 
+<!-- 网页样式 -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+	@import url("./assets/bower_components/bootstrap/dist/css/bootstrap.min.css");
+	@import url("./assets/bower_components/font-awesome/css/font-awesome.min.css");
+	@import url("./assets/bower_components/Ionicons/css/ionicons.min.css");
+	@import url("./assets/dist/css/AdminLTE.min.css");
+	@import url("./assets/dist/css/skins/_all-skins.min.css");
 </style>
